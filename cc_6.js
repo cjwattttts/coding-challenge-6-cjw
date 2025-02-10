5dab0d85487bbdab600ed729d094916d5a0b31a5
cjwattttts <cameron.jwatts@outlook.com>
2025-02-09 20:51:02 -0500

    Created a function to calculate business profit based on cost price, selling price, and units sold.

---
+++ cc_6.js
@@ -0,0 +1,7 @@
+//TASK 1 - Business Profit Calculation
+function calculateProfit(costPrice, sellingPrice, unitSold) { // declares function to calculate profit
+    let profit = (sellingPrice - costPrice) * unitSold; // formula for profit calculation
+    return "Profit Total: $" + profit;
+}
+console.log(calculateProfit(20, 30, 100)); // Expected output: "Total Profit: $1000"
+console.log(calculateProfit(50, 70, 200)); // Expected output: "Total Profit: $4000"
