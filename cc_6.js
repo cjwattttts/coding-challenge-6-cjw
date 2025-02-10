ed196e6404d7453132357629fbb105202b90b21e
cjwattttts <cameron.jwatts@outlook.com>
2025-02-09 21:10:21 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -8,10 +8,26 @@ console.log(calculateProfit(20, 30, 100)); // Expected output: "Total Profit: $1
 console.log(calculateProfit(50, 70, 200)); // Expected output: "Total Profit: $4000"
 
 //TASK 2 - Calculate Sales Tax
-const calulcateSalesTax = function(amount,taxRate) {
-    let tax = amount * taxRate;
-    return "Sales Tax: $" + tax;
+const calulcateSalesTax = function(amount,taxRate) { 
+    let tax = amount * taxRate; // multiplies amount by tax rate
+    return "Sales Tax: $" + tax; // returns tax calculation
 };
 
 console.log(calculateSalesTax(100, 0.07)); // Expected output: "Sales Tax: $7"
 console.log(calculateSalesTax(500, 0.1)); // Expected output: "Sales Tax: $50"
+
+//TASK 3 - Calculate Employee Bonus
+const calculateBonus = (salary, perfomanceRating) => {
+    let bonus; // declares bonus 
+    if (perfomanceRating === "Excellent") { // checks if perfomance is excellent
+        bonus = salary * 0.2; // 20 percent bonus
+    } else if (perfomanceRating === "Good") { // checks if perfomance is good
+        bonus = salary * 0.1; // 10 percent bonus
+    } else if (perfomanceRating === "Average") { // checks if perfomance is avergae
+        bonus = salary * 0.05; // 5 percent bonus
+    } 
+    return "Bonus: $" + bonus; // returns the bonus
+};
+
+console.log(calculateBonus(5000, "Excellent"));
+console.log(calculateBonus(7000, "Good"));
