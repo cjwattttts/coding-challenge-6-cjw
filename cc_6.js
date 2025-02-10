f0bdea85fb4845e51520be82ffb0659be43ec0cb
cjwattttts <cameron.jwatts@outlook.com>
2025-02-09 20:52:01 -0500

    Task 1 - Business Profit Calculation

--- cc_6.js
+++ cc_6.js
@@ -5,3 +5,4 @@ function calculateProfit(costPrice, sellingPrice, unitSold) { // declares functi
 }
 console.log(calculateProfit(20, 30, 100)); // Expected output: "Total Profit: $1000"
 console.log(calculateProfit(50, 70, 200)); // Expected output: "Total Profit: $4000"
+
